4d98f1efe34d7407982d419d434b4ef5c3abe9c4
MAngelesGB <mgutierrez6@ucol.mx>
2025-03-13 02:53:21 -0600

    force to implement content type on middleware

--- source/middlewares/index.js
+++ source/middlewares/index.js
@@ -5,8 +5,13 @@ const setUpHearders = require('./hearder');
 const setUpStaticAssets = require('./staticAssets');
 const setUpSwagger = require('./swagger');
 const setUpSeo = require('./seoConfig');
+
 //Ejecuta los middlewares importados
 const setUpMiddlewares = (app)=>{
+  app.use((req, res, next) => {
+    res.setHeader('X-Content-Type-Options', 'nosniff');
+    next();
+  });
   //Configuracion de seguridad en headers
   setUpHearders(app);
   //Configura la autenticacion de usuarios
@@ -19,6 +24,8 @@ const setUpMiddlewares = (app)=>{
   setUpSwagger(app);
   // Configuración del SEO para la renderización de las vistas
   app.use(setUpSeo);
+
+ 
 }
 
 module.exports = setUpMiddlewares;
